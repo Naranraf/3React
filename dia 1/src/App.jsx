ebc1af50f2f1b72ef2fa0dd8cab9164e0df5b360
Rafael Naranjo <106176753+Naranraf@users.noreply.github.com>
2023-05-16 13:15:00 -0600

    Initial commit

---
+++ dia 1/src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import ProductsForm from './components/ProductsForm'
+import axios from 'axios';
+
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+      <ProductsForm />
+    </>
+  )
+}
+
+export default App
