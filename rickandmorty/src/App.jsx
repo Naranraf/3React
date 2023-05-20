c4d8d43283e251f142954dee06fce88109bd105a
Rafael Naranjo <106176753+Naranraf@users.noreply.github.com>
2023-05-19 19:10:07 -0600

    ig

---
+++ rickandmorty/src/App.jsx
@@ -0,0 +1,146 @@
+import axios from "axios";
+import css from "./App.css"
+import { useEffect, useState } from "react";
+import ResidentInfo from "./components/ResidentInfo";
+import ResidentInfoDos from "./components/ResidentInfoDos";
+
+function App() {
+  const [location, setLocation] = useState({});
+  const [residents, setResidents] = useState([]);
+  const [currentPage, setCurrentPage] = useState(1);
+  const [idLocation, setIdLocation] = useState(3);
+
+  useEffect(() => {
+    axios
+      .get(`https://rickandmortyapi.com/api/location/${idLocation}`)
+      .then((resp) => {
+        console.log(resp.data);
+        setLocation(resp.data);
+        //setResidents( resp.data.residents )
+        getResidentsData(resp.data.residents);
+      })
+      .catch((error) => console.error(error));
+  }, [idLocation]);
+
+  const getResidentsData = (residentsArray) => {
+    const idArray = residentsArray.map((url) => url.split("/")[5]);
+
+    axios
+      .get(`https://rickandmortyapi.com/api/character/${idArray}`)
+      .then((resp) => {
+        setResidents(resp.data);
+      })
+      .catch((error) => console.error(error));
+  };
+
+  /*
+        10 personajes por pagina
+        pagina  indicePrimer    indiceUltimo
+        1           0           9
+        2           10          19
+        3           20          29
+        4           30          39
+
+
+        5 personajes por pagina
+        pagina  indicePrimer    indiceUltimo
+        1       0                4
+        2       5                9
+        3       10               14
+
+        residentPerPage = x
+        pag  indicePrimer    indiceUltimo
+        1    0               x - 1
+        numPage    (residentPerPage * numPage) - residentPerPage     numPage * residentPerPage 
+    */
+
+  const residentPerPage = 10;
+  const indexOfLastResident = residentPerPage * currentPage;
+  const indexOfFirstResident = indexOfLastResident - residentPerPage;
+  const currentResidents = residents.slice(
+    indexOfFirstResident,
+    indexOfLastResident
+  );
+  const lastPage = Math.ceil(residents.length / residentPerPage);
+
+  const pagination = (pageNumber) => {
+    setCurrentPage(pageNumber);
+  };
+
+  const [locationsResults, setLocationResults] = useState([]);
+  const getLocationsFiltered = (searchValue) => {
+    axios
+      .get(`https://rickandmortyapi.com/api/location?name=${searchValue}`)
+      .then((resp) => {
+        console.log(resp.data);
+        setLocationResults(resp.data.results);
+      });
+  };
+
+  return (
+    <div className="appContainer">
+      <h1>{location.name}</h1>
+
+      <input
+        type="text"
+        onChange={(e) => getLocationsFiltered(e.target.value)}
+      />
+      <select
+        name="locations"
+        id="locations"
+        onChange={(e) => setIdLocation(e.target.value)}
+      >
+        {locationsResults.map((location) => (
+          <option value={location.id}> {location.name} </option>
+        ))}
+      </select>
+
+      {/*
+            residents.map( resident => (
+                <ResidentInfo 
+                key={ resident }
+                url={ resident }
+                />
+            ))
+            */}
+      {currentResidents.map((resident) => (
+        <ResidentInfoDos key={resident.id} residentData={resident} />
+      ))}
+      <button
+        onClick={() => pagination(currentPage - 1)}
+        disabled={currentPage === 1}
+      >
+
+        <div className="pagination-buttons">
+
+        </div>
+        <ResidentInfo></ResidentInfo>
+
+
+
+      </button>
+      <div className="pagination-buttons">
+        Prev
+        {[1, 2, 3].map((number) => (
+          <button>
+
+            {number}
+
+          </button>
+        ))}
+        <button
+          onClick={() => pagination(currentPage + 1)}
+          disabled={currentPage === lastPage}
+        >
+          Next
+        </button>
+
+      </div>
+
+
+
+    </div>
+  );
+}
+
+export default App;
---
+++ rickandmorty/src/components/ResidentInfo.jsx
@@ -0,0 +1,24 @@
+
+import axios from 'axios'
+import { useState, useEffect } from 'react'
+
+const ResidentInfo = ({ url }) => {
+
+    const [ resident, setResident ] = useState( {} )
+
+    useEffect( () => {
+        axios
+        .get( url )
+        .then( resp => setResident(resp.data) )
+        .catch( error => console.error(error) )
+    }, [] )
+
+    return (
+        <div className="residentInfo-card">
+            <h1>{ resident.name }</h1>
+            <img src={ resident.image } alt="" />
+        </div>
+    );
+};
+
+export default ResidentInfo;
